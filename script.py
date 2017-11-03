facaa9e96580d7375c89fa727ba181e144da3a56
afishberg <fishberg.github@gmail.com>
2017-11-03 00:16:48 -0400

    init implementation of freq discriminator

--- script.py
+++ script.py
@@ -16,9 +16,6 @@ import matplotlib.pyplot as plt
 Generates 5 seconds worth of sampling of a 4Hz sin wave at 16 samples/sec
 Shows plot of sin samples
 Shows plot of FFT of sin
-
-Question: Why does it always show 0.5 peaks at -pi/2 and pi/2 instead of -pi/4
-and pi/4?
 """
 def test_fft():
     fs = 16
@@ -35,8 +32,6 @@ def test_fft():
 
 """
 Plots a provided FFT
-
-Question: Doesn't seem to work? I must misunderstand what FFT returns?
 """
 def plot_fft(X,title='FFT',normalize=False):
     # normalizes the fft
@@ -57,6 +52,9 @@ def plot_fft(X,title='FFT',normalize=False):
     #axes.set_xlim([-1,1])
     plt.show()
 
+def plot_freq_response(x):
+    plot_fft(fftshift(fft(x)))
+
 """
 Code I found on the internet for testing generated remez filter
 """
@@ -71,9 +69,6 @@ def plot_filter_response(h):
 
 """
 Generates a remez filter and then shows freq response and my fft function
-
-Question: Shouldn't these be the same?
-Question: Is my fft wrapping?
 """
 def test_filter():
     lpf = signal.remez(256, [0, 0.1, 0.125, .5], [1, 0])
@@ -95,27 +90,79 @@ def dt_channel_select(x,w0):
     n = np.arange(len(x))
     baseband = x * np.exp(-1j*w0*n)
 
-    # low-pass filter
-    #lpf = signal.remez(1024, [0,1/9,1/8,1],[1,0])
-    #lpf = signal.remez(72, [0, 0.1, 0.2, 0.4, 0.45, 0.5], [0, 1, 0])
-    lpf = signal.remez(256, [0, 0.1, 0.125, .5], [1, 0])
-    plot_filter_response(lpf)
+    #plot_freq_response(baseband)
 
-    X = np.fft.fftshift(np.fft.fft(lpf))
-    plot_fft(X,'lpf')
+    # low-pass filter
+    #lpf = signal.remez(1024, [0, 0.048, 0.05, .5], [1, 0])
+    lpf = signal.remez(512, [0, 100e3, 128e3, 1.024e6], [1, 0], Hz=2.048e6)
+    #plot_freq_response(lpf)
+
+    filtered  = signal.convolve(baseband,lpf)
+    decimated = filtered[0::8]
+
+    return decimated
+
+# ==============================================================================
+
+def freq_discriminator(x):
+    limited = limiter(x)
+
+    top = differentiator_top(limited)
+    bot = differentiator_bot(limited)
+
+    y2 = top*bot
+    # TODO Shouldn't this be only Im() at this point? Currently has Re() and Im().
+    #print(y2)
+    im = np.imag(y2)
+    return im
+
+def limiter(x):
+    mag = np.absolute(x)
+    y = x/mag
+    return y
+
+# TODO make ensure TOP/BOT use same M
+def differentiator_top(x):
+    ddt = signal.remez(512, [0, 128e3], [1], Hz=256e3, type='differentiator')
+    #plot_freq_response(ddt)
+    filtered = signal.convolve(x,ddt)
+    return filtered[0:-511]
+
+def differentiator_bot(x):
+    conj = np.conj(x)
+
+    freqs = np.arange(0,2*pi,2*pi/len(x))
+    delay = np.exp(-1j*freqs*511)
+    return conj*delay
+
+# ==============================================================================
+
+def deemphasis_filter(x):
+    None
+
+# ==============================================================================
+
+def mono_select(x):
+    None
+
+# ==============================================================================
 
 # ==============================================================================
 
 def demod(infile, w0):
     # load data
     samples = scipy.fromfile(open(infile), dtype=scipy.complex64)
-    samples = samples[0:len(samples)//20] # only use 0.5 sec of data for quick dbg
 
-    dt_channel_select(samples,w0)
+    x = samples[0:len(samples)//20] # only use 0.5 sec of data for quick dbg
+    #plot_freq_response(x)
+
+    y = dt_channel_select(x,w0)
+
+    m = freq_discriminator(y)
 
 if __name__ == '__main__':
-    # demod('blind_test.raw', -1.2916)
+    demod('blind_test.raw', -1.2916)
 
     # tests
     #test_fft()
-    test_filter()
+    #test_filter()
